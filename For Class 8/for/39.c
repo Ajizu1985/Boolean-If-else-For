8e71d6a78fc2888fed1bceafff7d650f1540c5ea
Ajizu1985 <az3406.aa@gmail.com>
2021-12-13 00:20:07 +0500

    Commit

---
+++ For Class 8/for/39.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    int A=0, B=5;
+
+    for(int i=A; i<=B; i++){
+        for(int j=1; j<=i; j++){
+    printf("%d\n", i);
+    } 
+    }
+    return 0;
+}
